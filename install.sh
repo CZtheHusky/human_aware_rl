0033db11aa77e8e851e3b00f11f7937fc527fb87
CZtheHusky <huskyc@qq.com>
2022-08-29 14:32:42 +0800

    add docker file

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # Install git-lfs for OSX
+pip config set global.index-url https://mirrors.aliyun.com/pypi/simple
 if [[ "$OSTYPE" =~ ^darwin ]]; then
   if hash git lfs 2>/dev/null; then
         git lfs install
@@ -21,3 +22,5 @@ cd ..
 pip install -e .
 
 conda install protobuf -y
+pip install tensorflow-gpu==2.4.1
+bash /human_aware_rl/run_tests.sh
